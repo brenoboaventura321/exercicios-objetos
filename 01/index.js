ff1b83b277f6c17eebb605db9a7bb9e7c2129a93
Breno Boaventura <99199296+brenoboaventura321@users.noreply.github.com>
2022-12-19 15:48:50 -0300

    exercicio 01

---
+++ 01/index.js
@@ -0,0 +1,20 @@
+const endereco = {
+  rua: "rua", 
+  numero: 101,
+  complemento: "ao lado do posto lado",
+  bairro: "bairro do posto",
+  cep: "0101010-010",
+  cidade: "Municipio do Posto",
+  estado: "São Posto",
+  pais: "Brazilis",
+}
+
+const pessoa ={
+  nome: "Joaquin",
+  email:"email_do_joaquim@joaquinet.com" ,
+  telefone: "01 8597-8528",
+  data_nascimetno: "16/16/2016",
+  endereco: endereco,
+}
+
+console.log(pessoa.endereco);
