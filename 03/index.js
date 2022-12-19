575b4c506cd5c78691be5e7505a07a31553bf522
Breno Boaventura <99199296+brenoboaventura321@users.noreply.github.com>
2022-12-19 15:56:30 -0300

    exercicio 03

---
+++ 03/index.js
@@ -0,0 +1,8 @@
+const usuario = {
+    nome: "João",
+    idade: 25,
+    profissao: "programador",
+    cor_preferida: "verde",
+}
+
+console.log(`Olá... Eu sou ${usuario.nome}, tenho ${usuario.idade} anos de idade, sou ${usuario.profissao} e minha cor cor preferida é ${usuario.cor_preferida}`)
