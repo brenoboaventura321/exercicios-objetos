f04831bcc21775bfca06162b6af74bb3a430a607
Breno Boaventura <99199296+brenoboaventura321@users.noreply.github.com>
2022-12-20 16:34:04 -0300

    exercicio 09

---
+++ 09/index.js
@@ -0,0 +1,29 @@
+const participantes = [
+    { nome: "João" },
+    { nome: "Ana" },
+    { nome: "Beatriz" },
+    { nome: "Maria" },
+    { nome: "Ana Clara" },
+    { nome: "Joana" },
+    { nome: "Augusto" },
+    { nome: "Renan" },
+    { nome: "Patricia" },
+    { nome: "Carlos" },
+    { nome: "Renato" },
+    { nome: "José" },
+    { nome: "Roberto" },
+    { nome: "Sara" },
+    { nome: "Junior" },
+    { nome: "Pedro" },
+    { nome: "Vitor" },
+    { nome: "Antonio" },
+];
+
+let posicao = 1;
+for (const lugar of participantes) {
+  if (lugar.nome === "Carlos") {
+    console.log(`Carlos esta na posição ${posicao}`)
+  }else {
+    posicao ++;
+  }
+}
